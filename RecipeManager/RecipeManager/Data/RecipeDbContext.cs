f283c7940dec1515fbf185224e2741458eac4058
Clarice Costello <claricostello@outlook.com>
2019-05-28 13:09:23 -0700

    Created RecipeDbContext with constructor

---
+++ RecipeManager/RecipeManager/Data/RecipeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Data
+{
+    public class RecipeDbContext : DbContext
+    {
+        public RecipeDbContext(DbContextOptions<RecipeDbContext> options): base(options)
+        {
+
+        }
+    }
+}
