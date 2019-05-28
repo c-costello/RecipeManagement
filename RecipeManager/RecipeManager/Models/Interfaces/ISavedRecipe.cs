258d90cbd775540c458b25fdc7a8b7ca3ce426fa
Clarice Costello <claricostello@outlook.com>
2019-05-28 15:21:30 -0700

    Created abbreviated ISavedRecipe interface with only create and delete methods

---
+++ RecipeManager/RecipeManager/Models/Interfaces/ISavedRecipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Models.Interfaces
+{
+    interface ISavedRecipe
+    {
+        Task<SavedRecipes> CreateSavedRecipes(int recipeID, string user);
+        Task DeleteRecipe(int recipeID, string user);
+    }
+}
