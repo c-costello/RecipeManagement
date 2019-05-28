920f15da5c3a822ef4adf3e614b041e9731a89fc
Clarice Costello <claricostello@outlook.com>
2019-05-28 13:05:20 -0700

    Created SavedRecipes model

---
+++ RecipeManager/RecipeManager/Models/SavedRecipes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Models
+{
+    public class SavedRecipes
+    {
+        public string User { get; set; }
+        public IEnumerable<Recipe> Recipes { get; set; }
+    }
+}
