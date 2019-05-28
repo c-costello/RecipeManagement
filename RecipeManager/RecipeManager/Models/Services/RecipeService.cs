1fb18ac14d191d9ad24e4fe9c1090333dd4a55bb
Clarice Costello <claricostello@outlook.com>
2019-05-28 15:40:08 -0700

    Implemented IRecipe interface in RecipeService

---
+++ RecipeManager/RecipeManager/Models/Services/RecipeService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeManager.Data;
+using RecipeManager.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Models.Services
+{
+    public class RecipeService : IRecipe
+    {
+        private readonly RecipeDbContext _context;
+
+        public RecipeService(RecipeDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Recipe> CreateRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+            return recipe;
+        }
+
+        public async Task DeleteRecipeById(int id)
+        {
+            Recipe recipe = await GetRecipeById(id);
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<Recipe> GetRecipeById(int id)
+        {
+            IEnumerable<Recipe> recipes = await _context.Recipes.ToListAsync();
+            Recipe recipe = recipes.FirstOrDefault(r => r.ID == id);
+            return recipe;
+        }
+
+        public async Task<IEnumerable<Recipe>> GetRecipes()
+        {
+            IEnumerable<Recipe> Recipes = await _context.Recipes.ToListAsync();
+            return Recipes;
+        }
+
+        public async Task<Recipe> UpdateRecipe(Recipe recipe)
+        {
+            _context.Recipes.Update(recipe);
+            await _context.SaveChangesAsync();
+            return recipe;
+        }
+    }
+}
