9a4f13fae8efa20789fbabeed79697e7e4e325c8
Clarice Costello <claricostello@outlook.com>
2019-05-28 12:59:45 -0700

    Added ID to Recipe Model

--- RecipeManager/RecipeManager/Models/Recipe.cs
+++ RecipeManager/RecipeManager/Models/Recipe.cs
@@ -7,6 +7,7 @@ namespace RecipeManager.Models
 {
     public class Recipe
     {
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int ServingSize { get; set; }
