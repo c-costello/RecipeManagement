c6c56846e3c6d6394f2c57a0b0c1c29afafcadd0
Clarice Costello <claricostello@outlook.com>
2019-05-28 13:21:07 -0700

    Registered Models in DbContext

--- RecipeManager/RecipeManager/Data/RecipeDbContext.cs
+++ RecipeManager/RecipeManager/Data/RecipeDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RecipeManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,12 @@ namespace RecipeManager.Data
         {
 
         }
+
+        public DbSet<SavedRecipes> SavedRecipes { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Instruction> Instructions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    
     }
 }
