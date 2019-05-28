1c133890f9d0865f3e15a7aebe3ddeef252487a1
Clarice Costello <claricostello@outlook.com>
2019-05-28 13:00:53 -0700

    Created Instruction Model

--- RecipeManager/RecipeManager/Models/Instruction.cs
+++ RecipeManager/RecipeManager/Models/Instruction.cs
@@ -7,6 +7,8 @@ namespace RecipeManager.Models
 {
     public class Instruction
     {
-        
+        public int RecipeID { get; set; }
+        public int StepNumber { get; set; }
+        public string Detail { get; set; }
     }
 }
