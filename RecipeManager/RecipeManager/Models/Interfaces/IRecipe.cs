ee6c5a3a6ec9fc24a37e17acd4cf126f3fce8a29
Clarice Costello <claricostello@outlook.com>
2019-05-28 15:09:04 -0700

    Built out IRecipe interface

---
+++ RecipeManager/RecipeManager/Models/Interfaces/IRecipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Models.Interfaces
+{
+    public interface IRecipe
+    {
+        Task<IEnumerable<Recipe>> GetRecipes();
+        Task<Recipe> GetRecipeById();
+        Task CreateRecipe();
+        Task DeleteRecipeById(int id);
+        Task UpdateRecipe(Recipe recipe);
+
+    }
+}
