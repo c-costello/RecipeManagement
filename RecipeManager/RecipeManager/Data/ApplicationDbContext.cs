fa39a6bc680483964052684aed41f3e3e204840e
Clarice Costello <claricostello@outlook.com>
2019-05-28 19:58:55 -0700

    Created IdentityDatabase

--- RecipeManager/RecipeManager/Data/ApplicationDbContext.cs
+++ RecipeManager/RecipeManager/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RecipeManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
