5c3e2a2fb4579e112831f049c99b01748576b366
Clarice Costello <claricostello@outlook.com>
2019-05-29 08:56:14 -0700

    Created constructor for account controller

---
+++ RecipeManager/RecipeManager/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeManager.Models;
+
+namespace RecipeManager.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _UserManager;
+        private readonly SignInManager<ApplicationUser> _SignInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _UserManager = userManager;
+            _SignInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel lvm)
+        {
+            return View();
+        }
+    }
+}
