526fee97b1598ee3f1c03ecb23dd8326f7236141
Clarice Costello <claricostello@outlook.com>
2019-05-31 15:47:22 -0700

    Tested methods to pass back multiple ingredients

---
+++ RecipeManager/RecipeManager/Controllers/RecipesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecipeManager.Models;
+using RecipeManager.Models.Interfaces;
+
+namespace RecipeManager.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly IIngredient _Ingredient;
+        private readonly IInstruction _Instruction;
+        private readonly IRecipe _Recipe;
+
+        public RecipesController(IIngredient ingredient, IInstruction instruction, IRecipe recipe)
+        {
+            _Ingredient = ingredient;
+            _Instruction = instruction;
+            _Recipe = recipe;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Recipe recipe)
+        {
+            var Instructions = recipe.Instructions;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ RecipeManager/RecipeManager/wwwroot/js/app.js
@@ -0,0 +1,6 @@
+﻿var ingCounter = 0;
+$('.ingredient-adder').on('click', () => {
+    ingCounter++;
+    console.log(ingCounter);
+    $(`<input type = "text" id = "Ingredients_${ingCounter}__Quantity" name = "Ingredients[${ingCounter}].Quantity" value = "" >\n <input type="text" id="Ingredients_${ingCounter}__Name" name="Ingredients[${ingCounter}].Name" value=""> \n<input type="text" id="Ingredients_${ingCounter}__Notes" name="Ingredients[${ingCounter}].Notes" value="">`).appendTo('.ingredients')
+})
