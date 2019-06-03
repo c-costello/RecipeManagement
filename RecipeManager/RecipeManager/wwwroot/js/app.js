40c6e6de7f8ead55c74fe4b322ff7b63321d52d2
Clarice Costello <claricostello@outlook.com>
2019-06-02 22:04:23 -0700

    changed ingredient form to table

--- RecipeManager/RecipeManager/wwwroot/js/app.js
+++ RecipeManager/RecipeManager/wwwroot/js/app.js
@@ -3,7 +3,8 @@ var instCounter = 0;
 $('.ingredient-adder').on('click', () => {
     ingCounter++;
     console.log(ingCounter);
-    $(`<input type = "text" id = "Ingredients_${ingCounter}__Quantity" name = "Ingredients[${ingCounter}].Quantity" value = "" >\n <input type="text" id="Ingredients_${ingCounter}__Name" name="Ingredients[${ingCounter}].Name" value=""> \n<input type="text" id="Ingredients_${ingCounter}__Notes" name="Ingredients[${ingCounter}].Notes" value="">`).appendTo('.ingredients')
+    $(`<tr class='test'> \n <td> \n <input type = "text" id = "Ingredients_${ingCounter}__Quantity" name = "Ingredients[${ingCounter}].Quantity" value = "" > \n </td>\n <td> <input type="text" id="Ingredients_${ingCounter}__Name" name="Ingredients[${ingCounter}].Name" value=""> \n </td> \n <td> \n <input type="text" id="Ingredients_${ingCounter}__Notes" name="Ingredients[${ingCounter}].Notes" value=""> \n </td> \n </tr>`).appendTo('.ingredients');
+
 })
 
 $('.instruction-adder').on('click', () => {
