e8df667b582ee034e9394060e2dd93eec0803a20
Clarice Costello <claricostello@outlook.com>
2019-06-03 09:17:50 -0700

    Created SavedRecipes controller with constructor

---
+++ RecipeManager/RecipeManager/Controllers/SavedRecipesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeManager.Models;
+using RecipeManager.Models.Interfaces;
+
+namespace RecipeManager.Controllers
+{
+    public class SavedRecipesController : Controller
+    {
+        private readonly ISavedRecipe _Context;
+        private readonly UserManager<ApplicationUser> _UserManager;
+
+        public SavedRecipesController(ISavedRecipe savedRecipe, UserManager<ApplicationUser> userManager)
+        {
+            _Context = savedRecipe;
+            _UserManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
