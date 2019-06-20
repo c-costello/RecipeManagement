74a4958bff19b722b98440b7e61faafa6bb8beeb
Clarice Costello <claricostello@outlook.com>
2019-06-20 14:22:44 -0700

    Added comments to instruction interface

--- RecipeManager/RecipeManager/Models/Interfaces/IInstruction.cs
+++ RecipeManager/RecipeManager/Models/Interfaces/IInstruction.cs
@@ -7,10 +7,35 @@ namespace RecipeManager.Models.Interfaces
 {
     public interface IInstruction
     {
+        /// <summary>
+        /// Async. Get all instructions related to a recipe
+        /// </summary>
+        /// <param name="recipeID">int</param>
+        /// <returns>List of Instructions</returns>
         Task<List<Instruction>> GetInstructions(int recipeID);
+        /// <summary>
+        /// Get Instruction by RecipeID and Step Number
+        /// </summary>
+        /// <param name="recipeID">int</param>
+        /// <param name="stepNumber">int</param>
+        /// <returns>Instruction</returns>
         Task<Instruction> GetInstruction(int recipeID, int stepNumber);
+        /// <summary>
+        /// Async. Add an instruction to the Instructions table
+        /// </summary>
+        /// <param name="instruction">Instruction</param>
+        /// <returns>Instruction</returns>
         Task<Instruction> CreateInstruction(Instruction instruction);
+        /// <summary>
+        /// Async. Update an instruction in the Instruction table.
+        /// </summary>
+        /// <param name="instruction">Instruction</param>
+        /// <returns>Instruction</returns>
         Task<Instruction> UpdateInstruction(Instruction instruction);
+        /// <summary>
+        /// Deletes an instruction from the Instruction table.
+        /// </summary>
+        /// <param name="instruction">Instruction</param>
         Task DeleteInstruction(Instruction instruction);
     }
 }
