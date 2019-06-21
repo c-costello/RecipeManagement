ba1c66f3d4d8a6e254352e77a80c96b00486a01c
Clarice Costello <claricostello@outlook.com>
2019-06-21 09:25:12 -0700

    Initial build

---
+++ RecipeManager/RecipeManagerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using RecipeManager.Models;
+
+namespace RecipeManagerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
