2ef6358c8f53af7356f8e7aded9bb4830d8f6161
Clarice Costello <claricostello@outlook.com>
2019-06-21 09:45:48 -0700

    Added setter tests to recipe gettera nd setter tests

--- RecipeManager/RecipeManagerTests/RecipeGettersAndSetters.cs
+++ RecipeManager/RecipeManagerTests/RecipeGettersAndSetters.cs
@@ -101,5 +101,109 @@ namespace RecipeManagerTests
             };
             Assert.Equal(list, recipe.Ingredients);
         }
+
+        [Fact]
+        public void CanSetID()
+        {
+            Recipe recipe = new Recipe()
+            {
+                ID = 1,
+            };
+            recipe.ID = 2;
+            Assert.Equal(2, recipe.ID);
+        }
+        [Fact]
+        public void CanSetTitle()
+        {
+            Recipe recipe = new Recipe()
+            {
+                Title = "string"
+            };
+            recipe.Title = "A different string";
+            Assert.Equal("A different string", recipe.Title);
+
+        }
+        [Fact]
+        public void CanSetDescription()
+        {
+            Recipe recipe = new Recipe()
+            {
+                Description = "string"
+            };
+            recipe.Description = "A different string";
+            Assert.Equal("A different string", recipe.Description);
+        }
+        [Fact]
+        public void CanSetNotes()
+        {
+            Recipe recipe = new Recipe()
+            {
+                Notes = "string"
+            };
+            recipe.Notes = "A different string";
+            Assert.Equal("A different string", recipe.Notes);
+        }
+
+        [Fact]
+        public void CanSetServingSize()
+        {
+            Recipe recipe = new Recipe()
+            {
+                ServingSize = 4
+            };
+            recipe.ServingSize = 3;
+            Assert.Equal(3, recipe.ServingSize);
+        }
+
+        [Fact]
+        public void CanSetIsSaved()
+        {
+            Recipe recipe = new Recipe()
+            {
+                IsSaved = false
+            };
+            recipe.IsSaved = true;
+            Assert.True(recipe.IsSaved);
+        }
+        [Fact]
+        public void CansetInstructions()
+        {
+            List<Instruction> list = new List<Instruction>();
+            Instruction instruction = new Instruction()
+            {
+                StepNumber = 1,
+                Detail = "This is a detail",
+                RecipeID = 1,
+            };
+            list.Add(instruction);
+            Recipe recipe = new Recipe()
+            {
+                Instructions = null
+            };
+            recipe.Instructions = list;
+            Assert.Equal(list, recipe.Instructions);
+        }
+        [Fact]
+        public void CanSetIngredients()
+        {
+            List<Ingredient> list = new List<Ingredient>();
+            Ingredient ingredient = new Ingredient()
+            {
+                ID = 1,
+                RecipeID = 1,
+                Name = "name",
+                Notes = "notes",
+                Quantity = "3 cups",
+
+
+            };
+            list.Add(ingredient);
+            Recipe recipe = new Recipe()
+            {
+                Ingredients = null
+            };
+            recipe.Ingredients = list;
+            Assert.Equal(list, recipe.Ingredients);
+        }
     }
 }
